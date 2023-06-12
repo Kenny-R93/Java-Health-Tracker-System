daf0580d50f0b271a00e3b219b1926e0e64fff49
Kenny-R93 <krangelunc@gmail.com>
2023-06-12 12:52:13 -0400

    It ain't much, but it's honest work

---
+++ src/main/java/org/example/Controller/UserController.java
@@ -0,0 +1,35 @@
+package org.example.Controller;
+
+import org.example.Model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserController {
+    private List<User> users;
+
+    public UserController(List<User> users) {
+        this.users = users;
+    }
+
+    public void addUser(User user) {
+        // before adding, we can check if user with this username already exists
+        if (getUser(user.getUsername()).isEmpty()) {
+            this.users.add(user);
+            // TODO: add code to persist the new user (write to file)
+        } else {
+            System.out.println("User with this username already exists.");
+        }
+    }
+
+    public Optional<User> getUser(String username) {
+        return users.stream()
+                .filter(u -> u.getUsername().equals(username))
+                .findFirst();
+    }
+
+    public List<User> getAllUsers() {
+        return users;
+    }
+}
+
